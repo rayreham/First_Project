a723c38fa46368cd927310d60ec1a962917e1bec
Reham <rayreham@yahoo.com>
2020-03-07 21:42:34 -0500

    First_Project

---
+++ responsive.js
@@ -0,0 +1,22 @@
+//this function in java script is used to navigate the page when clicking elements in bar
+function scrolling() {
+	scrollPos = $(this).scrollTop();
+	$('.p1').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+  $('.p2').css({
+		'background-position' : '50% ' + (-scrollPos/8)+"px"
+	});
+  $('.p3').css({
+		'background-position' : '70% ' + (-scrollPos/16)+"px"
+	});
+	$('.parallax-text').css({
+		'margin-top': (scrollPos/2)+"px",
+		'opacity': 1-(scrollPos/230)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		scrolling();
+	});
+});
